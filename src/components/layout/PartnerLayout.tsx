5b63efbb34862235dd9f42e6dc2e59f3b62e4197
Aamir Hashmi <aamirhashmi113@gmail.com>
2026-03-03 12:47:19 +0530

    fix partner header text visiblity on my-button class

--- src/components/layout/PartnerLayout.tsx
+++ src/components/layout/PartnerLayout.tsx
@@ -1,43 +1,40 @@
-'use client'
+"use client";
 
-import React from "react"
-import { PartnerSidebar } from './PartnerSidebar'
-import { PartnerHeader } from './PartnerHeader'
-import { useUser } from '@/hooks/useAuth'
-import { UnicsiLoader } from '../partner/unicsi-loader'
+import React from "react";
+import { PartnerSidebar } from "./PartnerSidebar";
+import { PartnerHeader } from "./PartnerHeader";
+import { useUser } from "@/hooks/useAuth";
+import { UnicsiLoader } from "../partner/unicsi-loader";
 
 export function PartnerLayout({ children }: { children: React.ReactNode }) {
   const { data: user, isLoading } = useUser();
 
   console.log("user-data==>123", user);
 
-  if(isLoading) return (
-     <UnicsiLoader />
-  )
+  if (isLoading) return <UnicsiLoader />;
 
   //shopify_access_token
-  if(!user?.data?.shopify_access_token && !user?.data?.shopify_store_url){
-    return (
-      <div className="flex h-screen bg-background">
-        {/* Main Content */}
-        <div className="flex-1 flex flex-col overflow-hidden">
-          {/* Header */}
-          <PartnerHeader />
+  // if(!user?.data?.shopify_access_token && !user?.data?.shopify_store_url){
+  //   return (
+  //     <div className="flex h-screen bg-background">
+  //       {/* Main Content */}
+  //       <div className="flex-1 flex flex-col overflow-hidden">
+  //         {/* Header */}
+  //         <PartnerHeader />
 
-          {/* Page Content */}
-          <main className="flex-1 overflow-y-auto">
-            <div className="p-6">{children}</div>
-          </main>
-        </div>
-      </div>
-    )
-  }
-
+  //         {/* Page Content */}
+  //         <main className="flex-1 overflow-y-auto">
+  //           <div className="p-6">{children}</div>
+  //         </main>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div className="flex h-screen bg-background">
       {/* Sidebar */}
-       <PartnerSidebar />
+      <PartnerSidebar />
 
       {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden">
@@ -50,5 +47,5 @@ export function PartnerLayout({ children }: { children: React.ReactNode }) {
         </main>
       </div>
     </div>
-  )
+  );
 }
